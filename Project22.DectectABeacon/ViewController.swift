d4ed7ae36f580fc0250c53b60248d840a2559e31
Scott Crowder <scrowder@gmail.com>
2024-06-03 12:16:02 -0500

    Initial Commit

---
+++ Project22.DectectABeacon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project22.DectectABeacon
+//
+//  Created by SCOTT CROWDER on 6/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
