3d0d90aef5a998d28f959bf0a7dd197f5a46708b
Scott Crowder <scrowder@gmail.com>
2024-06-03 12:21:04 -0500

    updating initial configuration

---
+++ Project22.DectectABeacon/MainVC.swift
@@ -0,0 +1,15 @@
+//
+//  MainVC.swift
+//  Project22.DectectABeacon
+//
+//  Created by SCOTT CROWDER on 6/3/24.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
